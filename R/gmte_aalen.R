5db2c5692e4677603ca1779ce2eab37cb486fc0e
Luke C. Pilling <luke.pilling@gmail.com>
2021-10-12 16:17:56 +0100

    Update gmte_aalen.R

--- R/gmte_aalen.R
+++ R/gmte_aalen.R
@@ -212,7 +212,7 @@ gmte_aalen = function(Y_t0,Y_t1,Y_d,T,G,Z,D,Nsim=100,alpha=0.05)
 
 	end_time = Sys.time()
 	time_taken = round(as.numeric(end_time-start_time),1)
-	cat("\nAnalysis completed in ", time_taken, " seconds\n")
+	cat(paste0("\nAnalysis completed in ", time_taken, " seconds\n"))
 
 	output_list=list(model="gmte_aalen",CAT=CATfit,GMTE0=GMTE0fit,GMTE1=GMTE1fit,RGMTE=RGMTEfit,MR=MRfit,FullCombined=FullCombined)
 	class(output_list)="twistR_GMTE"
